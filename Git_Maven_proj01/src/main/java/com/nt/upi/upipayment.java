1d4c784f9d922e0d4854d57704a2b0c64daea3e2
Amita parida <amritaparida28@gmail.com>
2026-08-16 06:51:32 +0530

    tl upi payment starter code

---
+++ Git_Maven_proj01/src/main/java/com/nt/upi/upipayment.java
@@ -0,0 +1,15 @@
+package com.nt.upi;
+
+public class upipayment {
+	public String dopayment(long phone , double amount)
+	{
+		System.out.println("UPI payment : dopayment()");
+		return amount+ "Amount is transferred to " + phone ;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
