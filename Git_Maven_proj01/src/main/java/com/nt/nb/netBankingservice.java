883ac5d67e423c93e8baad5d97a9abe2c70632e8
Amita parida <amritaparida28@gmail.com>
2026-08-17 18:11:55 +0530

    stashed commit

--- Git_Maven_proj01/src/main/java/com/nt/nb/netBankingservice.java
+++ Git_Maven_proj01/src/main/java/com/nt/nb/netBankingservice.java
@@ -4,6 +4,7 @@ public class netBankingservice {
 	
 	public String doPayment(long srcAcno , long destAcco, double amount) {
 		System.out.println("netBankingservice.doPayment()");
+		System.out.println("hello world");
 		System.out.println("adding new line to syso");
 		if(srcAcno <0 || destAcco < 0 || amount < 0)
 			throw new IllegalArgumentException("Invalid Input");
